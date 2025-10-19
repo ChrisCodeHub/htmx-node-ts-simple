e00eb80a66a3d797474194515c8cc56ae7f819c0
chris laptop <chris@example.com>
2025-10-19 20:04:37 +0100

    add some pngs

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,12 @@ import router from './routes/routes'
 const app = express();
 
 
-app.set('views', path.join(__dirname, '../views'));  // home for the pug templates
-app.set('view engine', 'pug');                       // and lets say "its pug templating"
+app.set('views', path.join(__dirname, '../views'));  // home for the pug templates, tells express 'where' the templates are
+app.set('view engine', 'pug');                       // and say "we're pug templating"
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
 app.use(express.json());
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', router);   // routes are tidied away in their own folder
 
